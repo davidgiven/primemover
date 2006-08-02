7d15e5e725c3059fa2d25563ddb3827a7238fcc9
dtrg <dtrg@localhost>
2006-08-02 22:42:37 +0000

    Switched on SVN keywords.

--- test-pmfiles/group.pm
+++ test-pmfiles/group.pm
@@ -1,6 +1,6 @@
--- $Id: shell 15 2006-07-25 09:21:06Z dtrg $
--- $HeadURL: /cvsroot/tack/Ack/pmfile,v $
--- $LastChangedDate: Exp $
+-- $Id$
+-- $HeadURL$
+-- $LastChangedDate$
 
 include "test-pmfiles/simple-c-program.pm"
 simple_c_program = default
--- test-pmfiles/multiple-install.pm
+++ test-pmfiles/multiple-install.pm
@@ -1,6 +1,6 @@
--- $Id: shell 15 2006-07-25 09:21:06Z dtrg $
--- $HeadURL: /cvsroot/tack/Ack/pmfile,v $
--- $LastChangedDate: Exp $
+-- $Id$
+-- $HeadURL$
+-- $LastChangedDate$
 
 include "lib/c.pm"
 
