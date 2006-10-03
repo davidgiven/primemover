8c1642bbbe2c272543740b83c081e294fcf33140
dtrg <dtrg@localhost>
2006-10-03 22:20:07 +0000

    Added unit tests.

---
+++ tests/startup.pm
@@ -0,0 +1,9 @@
+-- Tests that pm will start up and shut down.
+--
+-- © 2006 David Given.
+-- Prime Mover is licensed under the MIT open source license. Search
+-- for 'MIT' in this file to find the full license text.
+--
+-- $Id$
+
+os.exit()
