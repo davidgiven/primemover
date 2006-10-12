837def820f0131717fab10a73a362b71532221cf
dtrg <dtrg@localhost>
2006-10-12 20:08:32 +0000

    Moved the sample pmfiles and their supporting source files into the 'examples' directory.

--- examples/c-program-with-library.pm
+++ examples/c-program-with-library.pm
@@ -1,21 +1,21 @@
--- $Id: shell 15 2006-07-25 09:21:06Z dtrg $
--- $Source: /cvsroot/tack/Ack/pmfile,v $
--- $State: Exp $
+-- $Id$
+-- $HeadURL$
+-- $LastChangedDate$
 
 include "lib/c.pm"
 
-local d = "test-source/"
+DIR = "examples/source"
 
 lib = clibrary {
-	cfile (d.."test1.c"),
-	cfile (d.."test2.c"),
-	cfile (d.."test3.c"),
-	cfile (d.."test4.c")
+	cfile "%DIR%/test1.c",
+	cfile "%DIR%/test2.c",
+	cfile "%DIR%/test3.c",
+	cfile "%DIR%/test4.c"
 }
 
 default = cprogram {
-	cfile (d.."test.c"),
+	cfile "%DIR%/test.c",
 	lib,
 	
-	install = pm.install("test-source/c-program-with-library")
+	install = pm.install("%DIR%/c-program-with-library")
 }
--- examples/complex-c-program.pm
+++ examples/complex-c-program.pm
@@ -1,30 +1,29 @@
--- $Id: shell 15 2006-07-25 09:21:06Z dtrg $
--- $Source: /cvsroot/tack/Ack/pmfile,v $
--- $State: Exp $
+-- $Id$
+-- $HeadURL$
+-- $LastChangedDate$
 
 include "lib/c.pm"
 
-local d = "test-source/"
+DIR = "examples/source"
 
 default = cprogram {
-	CDEFINES = {"-Dtest1=fnord"},
+	CDEFINES = {"test1=fnord"},
 	
-	cfile (d.."test.c"),
-	cfile (d.."test1.c"),
+	cfile "%DIR%/test.c",
+	cfile "%DIR%/test1.c",
+	cfile "%DIR%/test2.c",
+	cfile "%DIR%/test3.c" (d.."test1.c"),
 	cfile (d.."test2.c"),
 	cfile (d.."test3.c"),
 	
 	-- Compile these two files with a modified definition of CDEFINES ---
-	-- they should get both test1 and test4 defined.
-	
-	cfile {
-		CDEFINES = {PARENT, "-Dtest4=test4A"},
-		d.."test4.c",
+	-- they should get bottest4=test4A"},
+		"%DIR%/test4.c",
 	},
 	cfile {
-		CDEFINES = {PARENT, "-Dtest4=test4B"},
-		d.."test4.c",
+		CDEFINES = {PARENT, "test4=test4B"},
+		"%DIR%/test4.c",
 	},
 	
-	install = pm.install("test-source/complex-c-program")
+	install = pm.install("examples/source/complex-c-program")
 }
