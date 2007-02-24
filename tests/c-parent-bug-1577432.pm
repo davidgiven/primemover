5d9b086fd78116dffb5306707024cf869b32f9c0
David Given <dg@cowlark.com>
2007-02-24 01:37:06 +0000

    c.pm wasn't always using self:__index() to load inherited property values, which meant that {PARENT...} wasn't being honoured in some situations.

---
+++ tests/c-parent-bug-1577432.pm
@@ -0,0 +1,19 @@
+-- $Id$
+-- $HeadURL$
+-- $LastChangedDate$
+
+include "../lib/c.pm"
+
+-- Create output files
+io.open("DATA.c", "w"):write([[
+int main(int argc, char* argv[]) { return 0; }
+]])
+
+CINCLUDES = {"."}
+
+default = cprogram {
+	cfile {
+		CINCLUDES = {PARENT, "."},
+		"DATA.c",
+	}
+}
