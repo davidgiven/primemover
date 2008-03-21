4e5b0f4ae9707c7568ae06f35260d910e9bfdf51
David Given <dg@cowlark.com>
2008-03-21 17:29:18 +0000

    Updated for version 0.1.4.

--- src/pm.lua
+++ src/pm.lua
@@ -11,7 +11,7 @@
 --                                GLOBALS                                  --
 -- ======================================================================= --
 
-local VERSION = "0.1.2.1"
+local VERSION = "0.1.4"
 
 -- Fast versions of useful system variables.
 
